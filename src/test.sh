2d7138ca00062a2f8bfe8b0037f5c615077cd7c1
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-04 16:00:13 +0100

    Updates experimental script

--- src/test.sh
+++ src/test.sh
@@ -1,15 +1,15 @@
 mydir=${0%/*}
 filename=$( basename "$0" )
 mandelbrot="./$mydir/../bin/mandelbrot"
-list=($( seq 2 -0.01 -2 | tr " " "\n" ))
+list=($( seq -2 0.01 2 | tr " " "\n" ))
 
-printf %s "$filename"
-
-for i in $( seq 0 200 )
+for i in $( seq 0 400 )
 do
     convert <(
-        "$mandelbrot" --julia-r "${list[$i]}" --width 480 --height 360 --bitmap --ratio 1 --iterations 10
+        "$mandelbrot" --julia-i "${list[$i]}" --julia-r "${list[$i]}" --multi-r "${list[$i]}" --multi-i "${list[$i]}" --width 480 --height 360 --bitmap --ratio 1 --iterations 10
     ) test_$( printf %05d $i ).png
+
+    printf %05d\n $i
 done
 
 ffmpeg -r 30 -i test_%05d.png -s hd720 -vcodec libx264 -crf 25 test.mp4
