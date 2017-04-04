07e31bb31445ea985d4cc149d549a6b3a5ec1704
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-04 13:50:35 +0100

    Adds working script, creates a proper animation

--- src/test.sh
+++ src/test.sh
@@ -1,11 +1,15 @@
 mydir=${0%/*}
 filename=$( basename "$0" )
 mandelbrot="./$mydir/../bin/mandelbrot"
-list=$( seq 0 20 )
+list=($( seq 2 -0.01 -2 | tr " " "\n" ))
 
 printf %s "$filename"
 
-#for i in $( seq 0 -0.1 -2 )
-#do
-#    convert <("$mandelbrot" --julia-r "$i" --width 480 --height 360 --bitmap --ratio 1) "list[$i]$filename.png"
-#done
+for i in $( seq 0 200 )
+do
+    convert <(
+        "$mandelbrot" --julia-r "${list[$i]}" --width 480 --height 360 --bitmap --ratio 1 --iterations 10
+    ) test_$( printf %05d $i ).png
+done
+
+ffmpeg -r 30 -i test_%05d.png -s hd720 -vcodec libx264 -crf 25 test.mp4
