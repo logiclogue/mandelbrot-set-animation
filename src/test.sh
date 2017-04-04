e204090c15c126f070049e2c3a11f186b8618ae3
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-04 21:04:39 +0100

    Tweaks

--- src/test.sh
+++ src/test.sh
@@ -1,12 +1,12 @@
 mydir=${0%/*}
 filename=$( basename "$0" )
 mandelbrot="./$mydir/../bin/mandelbrot"
-list=($( seq -2 0.01 2 | tr " " "\n" ))
+list=($( seq -4 0.01 4 | tr " " "\n" ))
 
-for i in $( seq 0 400 )
+for i in $( seq 0 800 )
 do
     convert <(
-        "$mandelbrot" --julia-i "${list[$i]}" --julia-r "${list[$i]}" --multi-r "${list[$i]}" --multi-i "${list[$i]}" --width 480 --height 360 --bitmap --ratio 1 --iterations 10
+        "$mandelbrot" --multi-r "${list[$i]}" --width 480 --height 360 --bitmap --ratio 1 --iterations 10
     ) test_$( printf %05d $i ).png
 
     printf "%05d\n" $i
