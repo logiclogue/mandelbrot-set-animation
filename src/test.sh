a8c08941ca716150de7108fe8240c27b5d82520d
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-04 07:55:28 +0100

    Tweaks

--- src/test.sh
+++ src/test.sh
@@ -1,10 +1,11 @@
 mydir=${0%/*}
 filename=$( basename "$0" )
 mandelbrot="./$mydir/../bin/mandelbrot"
+list=$( seq 0 20 )
 
 printf %s "$filename"
 
-for i in $( seq 0 -0.1 -2 )
-do
-    convert <($mandelbrot --julia-r "$i" --width 480 --height 360 --bitmap --ratio 1) "$i$filename.png"
-done
+#for i in $( seq 0 -0.1 -2 )
+#do
+#    convert <("$mandelbrot" --julia-r "$i" --width 480 --height 360 --bitmap --ratio 1) "list[$i]$filename.png"
+#done
