ef819c1680fbee7eee0631452cd0464685ec2543
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-04 16:20:31 +0100

    Fixes leading zeros issue

--- src/test.sh
+++ src/test.sh
@@ -9,7 +9,7 @@ do
         "$mandelbrot" --julia-i "${list[$i]}" --julia-r "${list[$i]}" --multi-r "${list[$i]}" --multi-i "${list[$i]}" --width 480 --height 360 --bitmap --ratio 1 --iterations 10
     ) test_$( printf %05d $i ).png
 
-    printf %05d\n $i
+    printf "%05d\n" $i
 done
 
 ffmpeg -r 30 -i test_%05d.png -s hd720 -vcodec libx264 -crf 25 test.mp4
