a98c32c8d664a62c4be8a7a640e21d2c2a49bb6b
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-05 20:58:57 +0100

    Updates to build to its own directory

--- src/test.sh
+++ src/test.sh
@@ -1,15 +1,22 @@
 mydir=${0%/*}
 filename=$( basename "$0" .sh )
+builddir="$mydir/../build/$filename"
 mandelbrot="./$mydir/../bin/mandelbrot"
-list=($( seq -4 0.01 4 | tr " " "\n" ))
+list=($( seq 0 0.01 8 | tr " " "\n" ))
+
+mkdir -p "$builddir"
 
 for i in $( seq 0 800 )
 do
     convert <(
-        "$mandelbrot" --multi-r "${list[$i]}" --width 480 --height 360 --bitmap --ratio 1 --iterations 10
-    ) "${filename}_$( printf %05d "$i" ).png"
+        "$mandelbrot" \
+            --bitmap --ratio 1 \
+            --multi-r "${list[$i]}" \
+            --width 100 --height 100 \
+            --iterations 10 "$@"
+    ) "$builddir/${filename}_$( printf %05d "$i" ).png"
 
     printf "%05d\n" "$i"
 done
 
-ffmpeg -r 30 -i "${filename}_%05d.png" -s hd720 -vcodec libx264 -crf 25 "$filename.mp4"
+ffmpeg -r 30 -i "$builddir/${filename}_%05d.png" -s hd720 -vcodec libx264 -crf 25 "$builddir/$filename.mp4"
