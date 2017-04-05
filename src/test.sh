2626a9ce74d139190802f9b64865a12990637d9b
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-05 19:41:09 +0100

    Fixes filename issue

--- src/test.sh
+++ src/test.sh
@@ -7,9 +7,9 @@ for i in $( seq 0 800 )
 do
     convert <(
         "$mandelbrot" --multi-r "${list[$i]}" --width 480 --height 360 --bitmap --ratio 1 --iterations 10
-    ) "test_$( printf %05d "$i" ).png"
+    ) "${filename}_$( printf %05d "$i" ).png"
 
     printf "%05d\n" "$i"
 done
 
-ffmpeg -r 30 -i test_%05d.png -s hd720 -vcodec libx264 -crf 25 "$filename.mp4"
+ffmpeg -r 30 -i "${filename}_%05d.png" -s hd720 -vcodec libx264 -crf 25 "$filename.mp4"
