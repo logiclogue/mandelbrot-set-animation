dc186334fcef4e026f320bc2a860128621eebbc4
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-05 19:37:12 +0100

    Tweaks to fix globbing

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,5 @@
 mydir=${0%/*}
-filename=$( basename "$0" )
+filename=$( basename "$0" .sh )
 mandelbrot="./$mydir/../bin/mandelbrot"
 list=($( seq -4 0.01 4 | tr " " "\n" ))
 
@@ -7,9 +7,9 @@ for i in $( seq 0 800 )
 do
     convert <(
         "$mandelbrot" --multi-r "${list[$i]}" --width 480 --height 360 --bitmap --ratio 1 --iterations 10
-    ) test_$( printf %05d $i ).png
+    ) "test_$( printf %05d "$i" ).png"
 
-    printf "%05d\n" $i
+    printf "%05d\n" "$i"
 done
 
-ffmpeg -r 30 -i test_%05d.png -s hd720 -vcodec libx264 -crf 25 test.mp4
+ffmpeg -r 30 -i test_%05d.png -s hd720 -vcodec libx264 -crf 25 "$filename.mp4"
