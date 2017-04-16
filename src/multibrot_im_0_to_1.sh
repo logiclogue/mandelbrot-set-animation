5378f70ffedcd13eb3c1077f516683514412ae6e
logiclogue <jordanpaullord@hotmail.co.uk>
2017-04-16 12:11:39 +0100

    Adds multibrot_im_0_to_1 animation

---
+++ src/multibrot_im_0_to_1.sh
@@ -0,0 +1,23 @@
+mydir=${0%/*}
+filename=$( basename "$0" .sh )
+builddir="$mydir/../build/$filename"
+mandelbrot="./$mydir/../bin/mandelbrot"
+list=($( seq 0 0.00056 1 | tr " " "\n" ))
+
+mkdir -p "$builddir"
+
+for i in $( seq 0 1800 )
+do
+    convert <(
+        "$mandelbrot" \
+            --bitmap --ratio 1 \
+            --width 100 --height 100 \
+            --multi-i "${list[$i]}" \
+            --iterations 10 "$@"
+    ) "$builddir/${filename}_$( printf %05d "$i" ).png"
+
+    printf "%05d\n" "$i"
+done
+
+ffmpeg -r 30 -i "$builddir/${filename}_%05d.png" -s hd720 -vcodec libx264 \
+    -crf 25 "$builddir/$filename.mp4"
